476d6d9caccd42f66b22ae4fc7432ae7156a578e
YOBD Digital <yobdmail@gmail.com>
2015-05-17 15:03:08 +0100

    Added plugin files

---
+++ readme.txt
@@ -0,0 +1,26 @@
+=== User Info In Admin Menu ===
+Contributors: josephbydesign
+Tags: user info, admin, menu, admin-menu, cms, avatar, logout, user profile, josephbydesign, YOBD Digital
+Requires at least: 3.0.1
+Tested up to: 4.2.2
+Stable tag: trunk
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+Add current user info at the top of the admin menu
+
+== Description ==
+
+Want to give you or your clients a better experience? This plugin will easily add the current user avatar and links to there profile and a logout link at the top of the Admin menu. This plugin is only for Admins not for general users. A great way to add a face lift to your or your clients backend!
+
+== Installation ==
+
+1. Upload `user_info_in_admin_menu.php` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Be awesome!
+
+== Screenshots ==
+
+1. `/assets/screenshot-1.png`
+
+
---
+++ user_info_in_admin_menu.php
@@ -0,0 +1,86 @@
+<?php
+/*
+Plugin Name: User Info in Admin Menu
+Description: Add current user info at the top of the admin menu
+Version: 1.0
+Author: Joseph VanTine
+License: GPL3
+*/
+
+/*
+Copyright 2015 Joseph VanTine
+ 
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 3, as
+published by the Free Software Foundation.
+ 
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+// Register style sheet.
+
+function yobd_user_admin_menu_info_plugin_styles() {
+	wp_register_style( 'user-info-in-admin-menu', plugins_url( 'assets/css/user_info_in_admin_menu_style.css' ) );
+	wp_enqueue_style( 'user-info-in-admin-menu' );
+}
+
+add_action( 'wp_enqueue_scripts', 'yobd_user_admin_menu_info_plugin_styles' );
+
+// Do your thing
+
+function yobd_user_admin_menu_info() {
+
+	global $current_user;
+
+	get_currentuserinfo();
+
+	echo '<link rel="stylesheet" href="' . plugins_url( 'assets/css/user_info_in_admin_menu_style.css', __FILE__ ) . '" > ';
+
+    ?>
+    <script type="text/javascript">
+        jQuery( document ).ready( function() {
+            jQuery( '#adminmenuwrap' ).prepend( '<?php 
+            	echo '<div class="admin_user_menu_info_wrap"><ul>';
+            		echo '<li>';
+	            		echo '<a href="' . get_edit_user_link() . '">'; 
+	            			//echo '<img class="admin_user_menu_info_av" src="' . plugins_url( 'assets/blank.jpg', __FILE__ ) . '">';
+	            			echo addslashes( get_avatar( $current_user->ID, 120, '', '', array ('class' => 'admin_user_menu_info_av') ) );
+	            		echo '</a>';
+	            	echo '</li>';
+
+	            	echo '<li>';
+	            		echo '<a href="' . get_edit_user_link() . '">'; 
+	            			echo '<p class="admin_user_menu_info_text admin_user_menu_info_name">';
+	            				echo 'Howdy, ' . $current_user->user_login . '';
+	            			echo '</p>';
+	            		echo '</a>';
+	            	echo '</li>';
+
+	            	echo '<li>';
+	            		echo '<p class="admin_user_menu_info_text admin_user_menu_info_logout">'; 
+	            			echo '<a href="' . get_edit_user_link() . '">Edit Profile</a>';
+	            			echo ' · ';
+	            			echo '<a href="' . wp_logout_url() . '">Logout</a>';
+	            		echo '</p>';
+	            	echo '</li>';
+
+            	echo '</ul></div>';
+            ?>');
+        });
+    </script>
+    <?php
+
+}
+
+
+	add_action( 'admin_footer', 'yobd_user_admin_menu_info' );
+
+
+?>
